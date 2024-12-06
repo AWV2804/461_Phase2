e55dcd659e3a8e4b4fd9139b1e75276aef93c0b2
nsykim <noahsykim@gmail.com>
2024-12-05 22:54:05 -0500

    fixed some issues

--- src/openAPI_controller.ts
+++ src/openAPI_controller.ts
@@ -75,8 +75,6 @@ const possibleReadmeFiles = [
     'README.rst',
     'README.markdown',
     'README.html',
-    'Readme.md',
-    'readme.md'
 ];
 
 const monkeyBusiness = '\"bearer 66abf860f10edcdd512e9f3f9fdc8af1bdc676503922312f8323f5090ef09a6a\"'
